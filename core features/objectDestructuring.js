d604aa82972c34e96bab06332fc1779a6d1bde73
yash mathur <yugsharma258258258@gmail.com>
2021-10-26 14:34:55 +0530

    objectDestructuring.js file has been added in core features folder

---
+++ core features/objectDestructuring.js
@@ -0,0 +1,7 @@
+const obj = {
+    userName:"Yash",
+    profile:"Programmer"
+}
+
+const {userName,profile} = obj          // This feature is called as object destructuring you can see how we are getting properties from this obj
+console.log(userName)
