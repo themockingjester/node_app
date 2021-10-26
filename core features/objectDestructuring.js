a42ecad51a31011e7dd4e9037da44b90da03d10e
yash mathur <yugsharma258258258@gmail.com>
2021-10-26 14:38:46 +0530

    objectDestructuring.js file has been updated in core features folder

--- core features/objectDestructuring.js
+++ core features/objectDestructuring.js
@@ -5,3 +5,5 @@ const obj = {
 
 const {userName,profile} = obj          // This feature is called as object destructuring you can see how we are getting properties from this obj
 console.log(userName)
+
+// Note : if we try to access those properties which are not available in object using object destructuring then program won't crash it will simply return undefined for such properties
